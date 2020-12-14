595bc3581a7f00768d6dcb9756533e090f356103
jazzyjackson <colten.jackson@lookalive.software>
2020-12-14 02:15:31 -0600

    recalc body every move (maybe would be better on mouseup)

--- source/js/artdrag.js
+++ source/js/artdrag.js
@@ -56,7 +56,21 @@ function handleMove(event){
 // }
 
 // so event.target is going ot be section, and all my cssvars on section.parentNode.style.cssVars
-
+function getCSSVars(article){
+	return {
+		"xunit": Number(article.style.getPropertyValue("--xunit")),
+		"yunit": Number(article.style.getPropertyValue("--yunit")),
+		"xcent": Number(article.style.getPropertyValue("--xcent")),
+		"ycent": Number(article.style.getPropertyValue("--ycent")),
+		"xstep": Number(article.style.getPropertyValue("--xstep")),
+		"ystep": Number(article.style.getPropertyValue("--ystep")),
+		"zoom":  Number(article.style.getPropertyValue("--zoom")),
+		"left":  Number(article.style.getPropertyValue("--left")),
+		"width":  Number(article.style.getPropertyValue("--width")),
+		"top":  Number(article.style.getPropertyValue("--top")),
+		"height":  Number(article.style.getPropertyValue("--height")),
+	}
+}
 
 function createUpdatePos(clientX, clientY, article){
 	//this is a function creator. When a mouse/touchdown event occurs, the initial position 
@@ -66,93 +80,140 @@ function createUpdatePos(clientX, clientY, article){
 	var theLastY = clientY;
 
   	let index = article.firstChild.id
-  	console.log("INDEX", index)
   	// click focus on whatever radio button is up there... it won't reload the page, no problem if already selected
   	form.querySelector(`[type="radio"][name="focus"][id="${index}"]`).click() // surely it will exist, right?
 
 
 	var globalzoom = root.contentDocument.body.style.getPropertyValue("--zoomg")
 
-	var props = {
-		"xunit": Number(article.style.getPropertyValue("--xunit")),
-		"yunit": Number(article.style.getPropertyValue("--yunit")),
-		"xcent": Number(article.style.getPropertyValue("--xcent")),
-		"ycent": Number(article.style.getPropertyValue("--ycent")),
-		"xstep": Number(article.style.getPropertyValue("--xstep")),
-		"ystep": Number(article.style.getPropertyValue("--ystep")),
-		"zoom":  Number(article.style.getPropertyValue("--zoom")),
+	var props = getCSSVars(article)
+
+  	return function(clientX, clientY, shift = false){
+		var movementX = clientX - theLastX;
+		var movementY = clientY - theLastY;
+		// compare how far I am from thelastx, overwrite the lastX once
+		// I'm measuring how far I've moved since I first clicked, and applying that distance to the position of the article when it was first clicked
+		let xunitadj = props.xunit * props.zoom * globalzoom
+		let yunitadj = props.yunit * props.zoom * globalzoom
+
+		let dxstep = Math.trunc(movementX / xunitadj)
+		let dystep = Math.trunc(movementY / yunitadj)
+
+		// now find the amount that can't be accounted for in numbers of wallpaper units
+		// gives a ratio 0-1 of how much of an xunit we've moved -- will roll over when its applied dxstep++, dystep++/--
+		let dxcent = (movementX % xunitadj) / xunitadj
+		let dycent = (movementY % yunitadj) / yunitadj
+
+	  	if(shift){
+	      	// will be 0 or 1 if current xcent plus change in xcent is greater than one
+
+	        let totalxcent = dxcent + props.xcent
+	        let totalycent = dycent + props.ycent
+
+	        if(totalxcent > 1){
+	        	dxstep++
+	        	dxcent = totalxcent - 1
+	        } else if(totalxcent < 0){
+	        	dxstep--
+	        	dxcent = totalxcent + 1
+	        } else {
+	        	dxcent = totalxcent
+	        }
+
+	        if(totalycent > 1){
+	        	dystep++
+	        	dycent = totalycent - 1
+	        } else if(totalycent < 0){
+	        	dystep--
+	        	dycent = totalycent + 1
+	        } else {
+	        	dycent = totalycent
+	        }
+
+	        // invert
+	        dycent = 1 - dycent
+
+			article.style.setProperty("--xcent", dxcent.toFixed(2))
+			article.style.setProperty("--ycent", dycent.toFixed(2))
+		    form.querySelector(`[name="--xcent-${index}"]`).value = dxcent.toFixed(2)
+		    form.querySelector(`[name="--ycent-${index}"]`).value = dycent.toFixed(2)
+	  	}
+		article.style.setProperty("--xstep", (props.xstep + dxstep).toFixed(0))
+		article.style.setProperty("--ystep", (props.ystep - dystep).toFixed(0))
+		form.querySelector(`[name="--xstep-${index}"]`).value = (props.xstep + dxstep).toFixed(0)
+		form.querySelector(`[name="--ystep-${index}"]`).value = (props.ystep - dystep).toFixed(0)
+
+		// calcMag(article)
+		recalcAll(article)
 	}
+}
 
-  	var enclosedUpdatePos = function(clientX, clientY, shift = false){
-      var movementX = clientX - theLastX;
-      var movementY = clientY - theLastY;
-      // overwrite enclosed variables
-      // theLastX = clientX;
-      // theLastY = clientY;
+function recalcAll(article){
+	let body = root.contentDocument.body
+	body.style.setProperty("--xmag", 0)	
+	body.style.setProperty("--ymag", 0)	
+	body.childNodes.forEach(art => calcMag(art))
+	// article.scrollIntoView()
+}
 
-      // compare how far I am from thelastx, overwrite the lastX once
+// on screen change, apply the updatebbox 
 
-      // I'm measuring how far I've moved since I first clicked, and applying that distance to the position of the article when it was first clicked
-      let xunitadj = props.xunit * props.zoom * globalzoom
-      let yunitadj = props.yunit * props.zoom * globalzoom
+// maybe with a debounce to start at 0 and recalculate all articles
 
-      let dxstep = Math.trunc(movementX / xunitadj)
-      let dystep = Math.trunc(movementY / yunitadj)
+// 
 
-      // now find the amount that can't be accounted for in numbers of wallpaper units
-      // gives a ratio 0-1 of how much of an xunit we've moved -- will roll over when its applied dxstep++, dystep++/--
-      let dxcent = (movementX % xunitadj) / xunitadj
-      let dycent = (movementY % yunitadj) / yunitadj
+// given an article, 
+// maybe recalculate all on zoom change?
 
-      console.log("BEFORE", {dxstep, dystep})
-
-  	if(shift){
-      	// will be 0 or 1 if current xcent plus change in xcent is greater than one
-
-        let totalxcent = dxcent + props.xcent
-        let totalycent = dycent + props.ycent
-
-        if(totalxcent > 1){
-        	dxstep++
-        	dxcent = totalxcent - 1
-        } else if(totalxcent < 0){
-        	dxstep--
-        	dxcent = totalxcent + 1
-        } else {
-        	dxcent = totalxcent
-        }
-
-        if(totalycent > 1){
-        	dystep++
-        	dycent = totalycent - 1
-        } else if(totalycent < 0){
-        	dystep--
-        	dycent = totalycent + 1
-        } else {
-        	dycent = totalycent
-        }
-
-        // invert
-        dycent = 1 - dycent
-
-        console.log("AFTER", {dxstep, dystep})
-
-		article.style.setProperty("--xcent", dxcent.toFixed(2))
-		article.style.setProperty("--ycent", dycent.toFixed(2))
-	    form.querySelector(`[name="--xcent-${index}"]`).value = dxcent.toFixed(2)
-	    form.querySelector(`[name="--ycent-${index}"]`).value = dycent.toFixed(2)
-  	}
-      // would have to store 
-
-      // console.log({dxstep, dystep, dxcent, dycent})
-
-    article.style.setProperty("--xstep", (props.xstep + dxstep).toFixed(0))
-    article.style.setProperty("--ystep", (props.ystep - dystep).toFixed(0))
-    form.querySelector(`[name="--xstep-${index}"]`).value = (props.xstep + dxstep).toFixed(0)
-    form.querySelector(`[name="--ystep-${index}"]`).value = (props.ystep - dystep).toFixed(0)
-
-  }
-  return enclosedUpdatePos;
-};
+// attach listener to the form, calcMag whenever position is changed
 
 
+function calcMag(article){
+
+	// magnitude only cares about embeds and texts
+	if(article.getAttribute("type") == "net") return null
+
+	let body = root.contentDocument.body
+
+	let props = getCSSVars(article)
+	var thismagx = Number(body.style.getPropertyValue("--xmag"))
+	var thismagy = Number(body.style.getPropertyValue("--ymag"))
+	
+	// set xmag and ymag
+
+  	let bboxtop = props.top
+        -   props.zoom
+        *   props.yunit
+        *   (props.ystep + props.ycent)
+
+    let bboxleft = props.left
+        -   props.zoom
+        *   props.xunit
+        *   (props.xstep + props.xcent)
+
+	body.style.setProperty(
+		"--xmag", 
+		Math.max(
+            thismagx || null, 
+            Math.abs(bboxleft),
+            Math.abs(
+            	bboxleft
+                + props.width
+                * props.zoom
+            )
+        )
+	)
+
+	body.style.setProperty(
+    	"--ymag",
+    	Math.max(
+            thismagy || null, 
+            Math.abs(bboxtop),
+            Math.abs(
+            	bboxtop
+                + props.height
+                * props.zoom
+            )
+        )
+    )
+}
